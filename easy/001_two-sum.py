5e67b0c80d6ff815af7fa20843b8f3e7b613f7af
lawrenceliu <lawrence.liu10@gmail.com>
2025-04-26 00:09:57 -0400

    two-sum solved

--- easy/001_two-sum.py
+++ easy/001_two-sum.py
@@ -6,22 +6,51 @@
 
 
 # brute-force solution, check all pairs
+# time: O(n^2)
+# space: O(1)
 def solution1(nums, target):
-    
+
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
-                result = {i, j}
+                result = [i, j]
                 return result
+            
+
+# hash-map solution, add a num and its index to a map
+# check each new num to see if its complement is in the map
+# time: O(n)
+# space: O(n)
+def solution2(nums, target):
+
+    indices = {}
+    indices[nums[0]] = 0;
+
+    for i in range(1, len(nums)):
+        complement = target - nums[i]
+        if complement in indices:
+            answer = [indices[complement], i]
+            return answer
+        else:
+            indices[nums[i]] = i
 
 
 
 
 def main():
-    nums = [2,7,11,15]
-    target = 9
+    nums1 = [2,7,11,15] #[0, 1]
+    target1 = 9
 
-    result = solution1(nums, target)
+    nums2 = [3,2,4] #[1,2]
+    target2 = 6
+
+
+    nums3 = [3,3] #[0,1]
+    target3 = 6
+
+    #result = solution1(nums1, target1)
+    result = solution2(nums3, target3)
+
     
     print(f"answer: {result}")
 
