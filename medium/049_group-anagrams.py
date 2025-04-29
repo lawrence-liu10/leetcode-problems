48e87133fd37850d189debb6e44af1bf0c4640dd
lawrenceliu <lawrence.liu10@gmail.com>
2025-04-28 23:39:55 -0400

    group anagrams hashmap-list solution done

---
+++ medium/049_group-anagrams.py
@@ -0,0 +1,43 @@
+# 049. Group Anagrams
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/group-anagrams/
+
+# time to solve: 15 min
+
+# make a hashmap, the key will be the hashmap of each word, the value will be a list
+# turn each word into a hashmap and check it against each key
+# if equal, append to the list
+
+# after a little research, hashmaps cannot be used as keys
+# we'll use tuples instead (different than hsm, order matters- 
+#   if we just tuple a string, the order will be different
+#   we first sort so the order will always be the same)
+
+# time complexity: O(n * m log(m)) b/c of sort, m being longest string len
+# space complexity: O(n * m) b/c each tuple can correspond to its own string
+def solution(strs: list[str]) -> list[list[str]]:
+    
+    anagrams = {}
+
+    for str in strs:
+        key = tuple(sorted(str))
+
+        if key not in anagrams:
+            anagrams[key] = []
+        anagrams[key].append(str)
+    
+
+    # return anagrams.values() <- returns the object, we just want the listed lists
+    return list(anagrams.values())
+
+def main():
+
+    strs1 = ["eat","tea","tan","ate","nat","bat"]
+    strs2 = [""]
+    strs3 = ["a"]
+
+    print(solution(strs1))
+
+if __name__ == "__main__":
+    main()
