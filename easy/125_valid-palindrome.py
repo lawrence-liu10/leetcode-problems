941c143aac9186fbd8b502279856f48117e2eaa8
lawrenceliu <lawrence.liu10@gmail.com>
2025-05-04 19:17:50 -0400

    add two pointer approach to valid palindrome

--- easy/125_valid-palindrome.py
+++ easy/125_valid-palindrome.py
@@ -3,8 +3,30 @@
 
 # Link: https://leetcode.com/problems/valid-palindrome/description/
 
-def solution():
-    pass
+# 2 pointer, move in until left and right are alphanumeric,
+# then check equality
+# messed up bounds, need l < r always
+# time complexity: O(n)
+# space complexity: O(1)
+def solution(s: str) -> bool:
+    
+    left = 0
+    right = len(s) - 1
+    s = s.lower()
+
+    while (left < right):
+        while not s[left].isalnum() and left < right:
+            left += 1
+        while not s[right].isalnum() and left < right:
+            right -= 1
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    
+    return True
+
+
 
 def main():
     pass
