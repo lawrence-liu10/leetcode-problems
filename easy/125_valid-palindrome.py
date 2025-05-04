fad55de0f43c162bc4090c3822d77a25b0dede1d
lawrenceliu <lawrence.liu10@gmail.com>
2025-05-04 19:11:17 -0400

    start valid palindrome

---
+++ easy/125_valid-palindrome.py
@@ -0,0 +1,13 @@
+# 125. Valid Palindrome
+# Difficulty: Easy
+
+# Link: https://leetcode.com/problems/valid-palindrome/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
