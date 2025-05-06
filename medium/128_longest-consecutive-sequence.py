9f431f9933a73e2346295e899e170255679209b6
lawrenceliu <lawrence.liu10@gmail.com>
2025-05-06 15:59:49 -0400

    start longest consecutive sequence

---
+++ medium/128_longest-consecutive-sequence.py
@@ -0,0 +1,13 @@
+# 128. Longest Consecutive Sequence
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+def solution(nums: List[int]) -> int:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
