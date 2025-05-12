3b166350bd73eea1b7c9bfabc1d82c145239bc53
loogway <lawrence.liu10@gmail.com>
2025-05-12 14:06:15 -0400

    add recursive solution to generate parentheses

--- medium/022_generate-parentheses.py
+++ medium/022_generate-parentheses.py
@@ -1,10 +1,45 @@
 # 022. Generate Parentheses
 # Difficulty: Medium
 
-# Link: 
+# Link: https://leetcode.com/problems/generate-parentheses/
 
-def solution():
-    pass
+# generate well-formed parentheses,
+# n <= 8, should we make a stack for each n?
+# for each new n, we can either pop and close or add a ( to the stack
+# how do we code this? recursion? idk how to do iteratively
+# each step has multiple possible substeps
+
+# conditions:
+# if # left paren = # right = n, add to solutions
+# if left < n, add a left
+# if right < left, add right
+
+# current solution has 5 outputs, but way too many parentheses
+# after each call to recursive, we need to pop to explore the other paths
+# time to solve: 40 min
+# space complexity: O(n)
+# time complexity: could not think of it, chatgpt says O(4^n/sqrt(n))
+def solution(n: int) -> list[str]:
+
+    stack = []
+    sols = []
+
+    def recursive(left, right):
+        if left == right == n:
+            sols.append("".join(stack))
+            return
+
+        if left < n:
+            stack.append("(")
+            recursive(left + 1, right)
+
+        if right < left:
+            stack.append(")")
+            recursive(left, right + 1)
+
+    recursive(0,0)
+    return sols
+
 
 def main():
     pass
