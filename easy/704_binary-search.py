1c19bee4fc3c5b0a0d56c6bdd4802b6d86955e4f
loogway <lawrence.liu10@gmail.com>
2025-05-15 15:24:07 -0400

    start binary search

---
+++ easy/704_binary-search.py
@@ -0,0 +1,13 @@
+# 704. Binary Search
+# Difficulty: Easy
+
+# Link: https://leetcode.com/problems/binary-search/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
