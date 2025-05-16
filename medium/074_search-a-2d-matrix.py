1fad7cbe284a5d1fff164fd6eba246b60689bf38
loogway <lawrence.liu10@gmail.com>
2025-05-16 16:43:08 -0400

    start search a 2d matrix

---
+++ medium/074_search-a-2d-matrix.py
@@ -0,0 +1,13 @@
+# 074. Search A 2D Matrix
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/search-a-2d-matrix/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
