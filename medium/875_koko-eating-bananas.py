466aa22b459a2b9688cfd27ccde1abb09f5bd706
loogway <lawrence.liu10@gmail.com>
2025-05-17 16:36:10 -0400

    start koko eating bananas

---
+++ medium/875_koko-eating-bananas.py
@@ -0,0 +1,13 @@
+# 875. Koko Eating Bananas
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/koko-eating-bananas/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
