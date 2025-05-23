2428bfb4ff2c0aa061ed9572f3cf86e50fd17bd0
loogway <lawrence.liu10@gmail.com>
2025-05-22 20:44:52 -0400

    start longest substring

---
+++ medium/003_longest-substring-without-repeating-characters.py
@@ -0,0 +1,13 @@
+# 003. Longest Substring Without Repeating Characters
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
