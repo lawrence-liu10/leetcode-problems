3f01ca55224ecaa73b53797028cbd12088763afd
lawrenceliu <lawrence.liu10@gmail.com>
2025-05-23 22:08:31 -0400

    start longest repeating char replacement

---
+++ medium/424_longest-repeating-character-replacement.py
@@ -0,0 +1,13 @@
+# 424. Longest Repeating Character Replacement
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/longest-repeating-character-replacement/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
