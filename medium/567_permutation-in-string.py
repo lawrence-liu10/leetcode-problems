1d3f900e2c68ef2ae7486f008b1e09d342b14547
loogway <lawrence.liu10@gmail.com>
2025-05-24 23:23:43 -0400

    start permutation in string

---
+++ medium/567_permutation-in-string.py
@@ -0,0 +1,13 @@
+# 567. Permutation In String
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/permutation-in-string/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
