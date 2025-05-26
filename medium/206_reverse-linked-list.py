b9da7e1f324ee2f491d670b6ac976cc39ad6e1be
loogway <lawrence.liu10@gmail.com>
2025-05-25 21:23:25 -0400

    start reverse linked list

---
+++ medium/206_reverse-linked-list.py
@@ -0,0 +1,13 @@
+# 206. Reverse Linked List
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/reverse-linked-list/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
