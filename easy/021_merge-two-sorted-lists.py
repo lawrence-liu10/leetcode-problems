52b3da71ce0b3a55bf973b9684f1b2a34cd55675
lawrenceliu <lawrence.liu10@gmail.com>
2025-05-27 02:13:12 -0400

    finish merge two sorted lists

--- easy/021_merge-two-sorted-lists.py
+++ easy/021_merge-two-sorted-lists.py
@@ -3,8 +3,39 @@
 
 # Link: https://leetcode.com/problems/merge-two-sorted-lists/description/
 
-def solution():
-    pass
+# simple, just iterate through both lists, if the first is <= 2nd, 
+# make temp = head, move list forward and iterate the current list
+# also need to keep track of head
+# took a bit to find bug, had list2 = list1.next, but otherwise simple
+# time complexity: O(n + m)
+# space complexity: O(1)
+# time to solve: 10 min
+
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def solution(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    
+    temp = ListNode()
+    head = temp
+    while list1 and list2:
+        if list1.val <= list2.val:
+            temp.next = list1
+            list1 = list1.next
+        else:
+            temp.next = list2
+            list2 = list2.next
+        temp = temp.next
+    
+    if list1:
+        temp.next = list1
+    if list2:
+        temp.next = list2
+    
+    return head.next
 
 def main():
     pass
