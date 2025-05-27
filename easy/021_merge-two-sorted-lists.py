63b5e2d08dbabf03c1e8d30e52ca9e6c3082d1a2
lawrenceliu <lawrence.liu10@gmail.com>
2025-05-27 01:59:11 -0400

    start merge two sorted lists

---
+++ easy/021_merge-two-sorted-lists.py
@@ -0,0 +1,13 @@
+# 021. Merge Two Sorted Lists
+# Difficulty: Easy
+
+# Link: https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
