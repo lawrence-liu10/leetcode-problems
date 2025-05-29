4defb3a8c886663332f0302ac858a5415e03787e
lawrenceliu <lawrence.liu10@gmail.com>
2025-05-28 20:00:26 -0400

    start reorder list

---
+++ medium/143_reorder-list.py
@@ -0,0 +1,19 @@
+# 143. Reorder List
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/reorder-list/description/
+
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
