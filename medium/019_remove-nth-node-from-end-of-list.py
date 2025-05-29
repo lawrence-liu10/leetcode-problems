5f6e8e6531a07898395312b7eb89a11f33bf74aa
loogway <lawrence.liu10@gmail.com>
2025-05-29 16:45:42 -0400

    start remove nth node from end of list

---
+++ medium/019_remove-nth-node-from-end-of-list.py
@@ -0,0 +1,13 @@
+# 019. Remove Nth Node From End Of List
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
