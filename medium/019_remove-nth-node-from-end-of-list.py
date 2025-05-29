0a728751bc3f22026014ffd25e1691b0a959b232
loogway <lawrence.liu10@gmail.com>
2025-05-29 17:03:48 -0400

    add two pointer solution to remove nth node

--- medium/019_remove-nth-node-from-end-of-list.py
+++ medium/019_remove-nth-node-from-end-of-list.py
@@ -3,8 +3,31 @@
 
 # Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
 
-def solution():
-    pass
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# we can use two pointers which are spaced n apart, and move the fast pointer to the end
+# then we skip the node after the slow pointer
+# we need to start at one before the head, otherwise the slow pointer will be one ahead
+
+def solution(head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    dummy = ListNode(0, head)
+    fast = dummy
+    slow = dummy
+
+    for _ in range(n + 1):
+        fast = fast.next
+
+    while fast:
+        fast = fast.next
+        slow = slow.next
+
+    slow.next = slow.next.next
+    return dummy.next
+
 
 def main():
     pass
