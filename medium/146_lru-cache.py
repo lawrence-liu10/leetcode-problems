67885087e7fb681010937e6e0ac5a46a7d75fb3b
loogway <lawrence.liu10@gmail.com>
2025-06-02 15:49:01 -0400

    start lru cache

---
+++ medium/146_lru-cache.py
@@ -0,0 +1,13 @@
+# 146. Lru Cache
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/lru-cache/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
