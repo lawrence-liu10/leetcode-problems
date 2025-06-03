aa5f8997b46629ec81cc6a783d7bba6b05a1d2ab
loogway <lawrence.liu10@gmail.com>
2025-06-03 19:28:38 -0400

    start invert binary tree

---
+++ easy/226_invert-binary-tree.py
@@ -0,0 +1,13 @@
+# 226. Invert Binary Tree
+# Difficulty: Easy
+
+# Link: https://leetcode.com/problems/invert-binary-tree/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
