c47127f4f2991da4c27e22b54ff050ee31ae051d
loogway <lawrence.liu10@gmail.com>
2025-06-04 19:16:15 -0400

    add recursive solution to max depth of binary tree

--- easy/104_maximum-depth-of-binary-tree.py
+++ easy/104_maximum-depth-of-binary-tree.py
@@ -10,8 +10,18 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def solution():
-    pass
+# we can do this recursively, just travel down the left and right subtree
+# base case of a null node, return 0
+# otherwise, return 1 + the max of the left or right subtree
+# time complexity: O(n)
+# space complexity: O(n)
+# time to solve: 5 min
+def solution(self, root: Optional[TreeNode]) -> int:
+
+    if not root:
+        return 0
+
+    return 1 + max(self.solution(root.left), self.solution(root.right))
 
 def main():
     pass
