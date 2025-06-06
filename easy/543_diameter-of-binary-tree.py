54c301bbc85a2cf9d9dcbc1fd326805b5e04aa4c
loogway <lawrence.liu10@gmail.com>
2025-06-06 00:10:53 -0400

    start diameter of binary tree

---
+++ easy/543_diameter-of-binary-tree.py
@@ -0,0 +1,20 @@
+# 543. Diameter Of Binary Tree
+# Difficulty: Easy
+
+# Link: https://leetcode.com/problems/diameter-of-binary-tree/description/
+
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
