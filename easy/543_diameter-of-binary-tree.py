bb6c1cc18268a0ed33a8c639bcaaa8f4b135a400
loogway <lawrence.liu10@gmail.com>
2025-06-06 00:48:47 -0400

    add recursive solution to diameter of binary tree

--- easy/543_diameter-of-binary-tree.py
+++ easy/543_diameter-of-binary-tree.py
@@ -10,8 +10,33 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def solution():
-    pass
+# the diameter at any node is the max of the left subtree, the right subtree, or
+#   the height of the left tree + the height of the right tree + 1
+# we can use this to implement a recursive solution
+
+# if a node is null, its height is zero
+# otherwise, we check the height of the left and right subtree, updating the diameter if that's larger than the current one
+# at each step, we return this max diameter + 1 (accounting for the current node)
+# time complexity: O(n)
+# space complexity: O(n)
+# time to solve: 30 min
+
+def solution(self, root: TreeNode) -> int:
+    self.diameter = 0
+
+    def height(node: TreeNode):
+        if not node:
+            return 0
+        left_height = height(node.left)
+        right_height = height(node.right)
+
+        self.diameter = max(self.diameter, left_height + right_height)
+
+        return 1 + max(left_height, right_height)
+
+    height(root)
+    return self.diameter
+
 
 def main():
     pass
