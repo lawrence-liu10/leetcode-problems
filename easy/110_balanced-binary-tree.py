3a53263d4b0dcfce048e68ff1f8d4a4bf775307a
loogway <lawrence.liu10@gmail.com>
2025-06-06 17:09:59 -0400

    add recursive solution to balanced binary tree

--- easy/110_balanced-binary-tree.py
+++ easy/110_balanced-binary-tree.py
@@ -10,10 +10,37 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def solution():
-    pass
+# going from top to bottom seems bad, we have to calculate the height of every subtree
+#   which leads to a lot of repeated steps
+# we need to work from the leaves up, we can do it recursively
+# from the leaves, we set the height to 0, and check if subtrees are balanced
+
+# base case, a null node, the subtree is balanced and the height is 0
+# recursive step, we check if the height of the l subtree and r subtree differ by <= 1, and if the children subtrees are balanced
+# if so, the current subtree will be balanced
+# time complexity: O(n)
+# space complexity: O(n)
+# time to solve: 40 min
+def solution(self, root: Optional[TreeNode]) -> bool:
+
+    def check(node):
+        if not node:
+            return 0, True
+
+        left_height, left_balanced = check(node.left)
+        right_height, right_balanced = check(node.right)
+
+        cur_height = 1 + max(left_height, right_height)
+        is_balanced = (left_balanced and right_balanced and
+                       abs(left_height - right_height) <= 1)
+
+        return cur_height, is_balanced
+
+    height, balanced = check(root)
+    return balanced
 
 def main():
+
     pass
 
 if __name__ == "__main__":
