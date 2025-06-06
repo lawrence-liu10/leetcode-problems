57f984c61b7f8c5fa7075898c25bfc40a12016cb
loogway <lawrence.liu10@gmail.com>
2025-06-06 16:29:17 -0400

    start balanced binary tree

---
+++ easy/110_balanced-binary-tree.py
@@ -0,0 +1,20 @@
+# 110. Balanced Binary Tree
+# Difficulty: Easy
+
+# Link: https://leetcode.com/problems/balanced-binary-tree/description/
+
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
