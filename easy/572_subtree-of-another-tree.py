2b6548b708256a396e3294270eca635e449392e3
loogway <lawrence.liu10@gmail.com>
2025-06-08 16:59:16 -0400

    start subtree of another tree

---
+++ easy/572_subtree-of-another-tree.py
@@ -0,0 +1,13 @@
+# 572. Subtree Of Another Tree
+# Difficulty: Easy
+
+# Link: https://leetcode.com/problems/subtree-of-another-tree/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
