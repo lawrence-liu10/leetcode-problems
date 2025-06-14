942658bf3ab22e694898615a8139ab71c3f84021
loogway <lawrence.liu10@gmail.com>
2025-06-14 00:55:55 -0400

    add recursive dfs and bfs solution to same tree

--- easy/100_same-tree.py
+++ easy/100_same-tree.py
@@ -11,7 +11,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
-# for this question we can do an iterative solution
+# for this question we can do an iterative stack solution
 # we first add the roots
 #   at each level after, we pop the nodes inside
 #   if they're null, skip
@@ -35,6 +35,51 @@ def solution(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         stack.append((p_node.right, p_node.right))
 
     return True
+
+# recursive dfs solution
+# couple of base cases, we reach the end point where both nodes are null, they're the same
+# if one is null and the other isn't, they're diff
+# if any value is different, they're different
+# and we recursively call on the children
+def solution(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+    if not p and not q:
+        return True
+    if not p or not q:
+        return False
+    if p.val != q.val:
+        return False
+    return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+# bfs solution, solve by level
+# for now, I'll check the same conditions but also make 2 queues, there's probably a more efficient way though
+# slightly different in this case, if both are the same we just keep checking
+# when we reach the end, they're completely the same
+def solution(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+    p_queue = deque(p)
+    q_queue = deque(q)
+
+    while p_queue and q_queue:
+        for _ in range(len(p_queue)):
+            p_node = p_queue.popleft()
+            q_node = q_queue.popleft()
+
+            if not p_node and not q_node:
+                continue
+
+            if not p_node or not q_node:
+                return False
+
+            p_queue.append(p_node.left)
+            p_queue.append(p_node.right)
+            q_queue.append(q_node.left)
+            q_queue.append(q_node.right)
+
+    return True
+
+
 
 
 
