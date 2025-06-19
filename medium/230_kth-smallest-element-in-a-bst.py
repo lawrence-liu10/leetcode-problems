8407763045b3d8b6c22c89208ab4c52e9ed15b90
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-19 18:06:48 -0400

    start kth smallest element in a bst

---
+++ medium/230_kth-smallest-element-in-a-bst.py
@@ -0,0 +1,13 @@
+# 230. Kth Smallest Element In A BST
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
