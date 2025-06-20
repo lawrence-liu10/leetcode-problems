595645a65ea1d4afead387062e36c3aae965eaef
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-20 17:38:39 -0400

    add iterative dfs solution to construct bst from pre and inorder traversal

--- medium/105_construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ medium/105_construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -11,8 +11,37 @@ class TreeNode:
 
 # Link: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
 
-def solution():
-    pass
+# preorder: NLR, inorder: LNR
+# I understand how to do it on paper, but how do you convert it to code
+# first and last node are always root and deepest on right
+# in an inorder traversal, we go all the way down the left path until we can't,
+# then we go down the left path of the right child
+
+# we'll keep going through the preorder list until it's equal to the inorder list vals
+# then we go through the inorder list to build the left tree until they differ
+# when they differ, we repeat with the next preorder value being the right child of the 
+# left tree we just built
+def solution(self, preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
+    
+    root = TreeNode(preorder[0])
+    inorder_idx = 0
+    stack = [root]
+
+    for i in range(1, len(preorder)):
+        node = TreeNode(preorder[i])
+
+        if stack[-1].val != inorder[inorder_idx]:
+            stack[-1].left = node
+        else: # go right, keep going down inorder list until they differ
+            while stack and stack[-1].val == inorder[inorder_idx]: 
+                parent = stack.pop()
+                inorder_idx += 1
+            parent.right = node
+        stack.append(node)
+    
+    return root
+
+
 
 def main():
     pass
