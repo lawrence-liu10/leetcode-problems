64712337029c37d64cdec8ada3ea47cb9c7c542e
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-20 16:41:35 -0400

    start construct bin tree

---
+++ medium/105_construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,21 @@
+# 105. Construct Binary Tree From Preorder And Inorder Traversal
+# Difficulty: Medium
+
+from collections import deque
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Link: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
