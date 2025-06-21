1693278b723646dc1f8e2a2e48afc74a97a81496
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-21 15:29:39 -0400

    start serialize and deserialize binary tree

---
+++ hard/297_serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,22 @@
+# 297. Serialize And Deserialize Binary Tree
+# Difficulty: Hard
+
+# Link: https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+
+from collections import deque
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
