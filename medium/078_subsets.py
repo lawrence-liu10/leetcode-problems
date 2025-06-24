428aa0e4c0ba5711fc0813e74951a071d8455c75
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-23 23:26:42 -0400

    add backtrack sol to subsets

--- medium/078_subsets.py
+++ medium/078_subsets.py
@@ -3,8 +3,24 @@
 
 # Link: https://leetcode.com/problems/subsets/description/
 
-def solution():
-    pass
+# backtracking problem
+# the general pattern to solving backtracking is, while the problem isn't solved,
+# we pick a path from the starting point, and if it's valid we recurse with it
+#   then 'unchoose' it before continuing
+
+def solution(self, nums: list[int]) -> list[list[int]]:
+    
+    def backtrack(start, path):
+        sol.append(path[:])
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(start + 1, path)
+            path.pop()
+    
+    sol = []
+    backtrack(0, [])
+    return sol
+    
 
 def main():
     pass
