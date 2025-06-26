caef48842531c1bb5585f3e86d689d4e0445d9e0
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-25 21:58:16 -0400

    add backtracking sol to combination sum II

--- medium/040_combination-sum-ii.py
+++ medium/040_combination-sum-ii.py
@@ -3,8 +3,36 @@
 
 # Link: https://leetcode.com/problems/combination-sum-ii/description/
 
-def solution():
-    pass
+# similar to combination sum one, but we can only use nums once
+# we can almost use the same solution, but right now duplicates cause errors
+# how do we avoid duplicates
+# when backtracking, we pass the next element to avoid using the same element twice
+# as well, if there's multiple of the same element, we only want to use each once
+#   so we skip consecutive duplicate elements UNLESS it's our first time seeing them
+#   if we skip without considering them we miss solutions
+
+def solution(self, candidates: list[int], target: int) -> list[list[int]]:
+    sol = []
+
+    def dfs(start, path, sum):
+        if sum == target:
+            sol.append(path.copy())
+            return
+
+        for i in range(start, len(candidates)):
+            if i > start and candidates[i] == candidates[i - 1]:
+                continue
+
+            if sum + candidates[i] > target:
+                break
+            
+            path.append(candidates[i])
+            dfs(i + 1, path, sum + candidates[i])
+            path.pop()
+    
+    dfs(0, [], 0)
+    return sol
+        
 
 def main():
     pass
