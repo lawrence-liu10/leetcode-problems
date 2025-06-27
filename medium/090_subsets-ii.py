07b7741bdea63e53055dd3ef2d94b27a72788383
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-27 14:52:25 -0400

    add backtracking sol to subsets II

--- medium/090_subsets-ii.py
+++ medium/090_subsets-ii.py
@@ -3,8 +3,29 @@
 
 # Link: https://leetcode.com/problems/subsets-ii/
 
+# this seems very similar to combination sum II
+# let's start by sorting to avoid duplicates
+# we implement the same logic from combination sum II, where if there's consecutive same numbers,
+#   we only count the first one in each recursive step to avoid duplicates
+# otherwise, we explore a new option and pop after each exploration
 def solution(self, nums: list[int]) -> list[list[int]]:
-    pass
+
+    sol = []
+    nums.sort()
+
+    def dfs(start, path):
+        sol.append(path.copy())
+
+        for i in range(start, len(nums)):
+            if nums[i] == nums[i - 1] and i != start:
+                continue
+
+            path.append(nums[i])
+            dfs(i + 1, path)
+            path.pop()
+    
+    dfs(0, [])
+    return sol
 
 def main():
     pass
