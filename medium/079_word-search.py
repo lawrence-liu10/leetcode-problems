4d9e437d3e103a6ad48ec7c33696b9293adb206c
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-28 23:26:27 -0400

    start word search

---
+++ medium/079_word-search.py
@@ -0,0 +1,13 @@
+# 079. Word Search
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/word-search/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
