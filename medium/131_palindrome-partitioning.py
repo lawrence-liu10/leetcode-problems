bac11d79581d104a4a70b0c76f2f82e829c17d27
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-30 11:17:53 -0400

    add backtracking solution to palindrome partitioning

--- medium/131_palindrome-partitioning.py
+++ medium/131_palindrome-partitioning.py
@@ -3,8 +3,30 @@
 
 # Link: https://leetcode.com/problems/palindrome-partitioning/description/
 
-def solution():
-    pass
+# choice
+#   we explore each substring starting from each index
+# constraint
+#   we check if substrings are palindromes and append to the current list if so
+# goal
+#   if our path reaches the string length, we have a solution
+def solution(self, s: str) -> list[list[str]]:
+    
+    sol = []
+    def backtrack(start, path):
+        if start == len(s):
+            sol.append(path.copy()) 
+            return
+        
+        for i in range(start + 1, len(s) + 1):
+            substring = s[start:i]
+            if substring == substring[::-1]:
+                path.append(substring)
+                backtrack(i + 1, path)
+                path.pop()
+    
+    backtrack(0, [])
+    return sol
+
 
 def main():
     pass
