a645b1537f50c6fb5c3ea57c61701c97b516a711
lawrenceliu <lawrence.liu10@gmail.com>
2025-06-30 11:18:06 -0400

    start letter combinatinos of a phone number

---
+++ medium/017_letter-combinations-of-a-phone-number.py
@@ -0,0 +1,13 @@
+# 017. Letter Combinations Of A Phone Number
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
+def solution(self, digits: str) -> list[str]:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
