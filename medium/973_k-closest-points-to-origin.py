269d3f3784e9906010c024d4fbcb6583e36a5c26
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-03 12:53:07 -0400

    start k closest points

---
+++ medium/973_k-closest-points-to-origin.py
@@ -0,0 +1,13 @@
+# 973. K Closest Points To Origin
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/k-closest-points-to-origin/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
