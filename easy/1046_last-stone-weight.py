a89b434de20f23cab89dfee9df752eaf7b93a39c
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-02 23:11:42 -0400

    start last stone weight

---
+++ easy/1046_last-stone-weight.py
@@ -0,0 +1,15 @@
+# 1046. Last Stone Weight
+# Difficulty: Easy
+
+import heapq
+
+# Link: https://leetcode.com/problems/last-stone-weight/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
