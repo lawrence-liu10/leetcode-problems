8312d8c26f8219c9de9a2431e5fc9d2f0142474d
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-03 13:13:37 -0400

    add max heap solution to k closest points

--- medium/973_k-closest-points-to-origin.py
+++ medium/973_k-closest-points-to-origin.py
@@ -1,10 +1,40 @@
 # 973. K Closest Points To Origin
 # Difficulty: Medium
 
+import math
+import heapq
+
 # Link: https://leetcode.com/problems/k-closest-points-to-origin/description/
 
-def solution():
-    pass
+# let's define a helper to calculate distances to make it a little cleaner
+
+# besides that, we'll make a heap of distances and points
+# we'll also keep track of the minimum distance we push into the array
+# for any new element, it must be at least that distance to append
+# we pop any time the heap has over k elements
+# and return at the end 
+def solution(self, points: list[list[int]], k: int) -> list[list[int]]:
+    
+    def dist(point: list[int]):
+        return math.sqrt(point[0] ** 2 + point[1] ** 2)
+    
+    max_heap = []
+    
+    for point in points:
+        distance = dist(point)
+        heapq.heappush(max_heap, [-distance, point])
+        if len(max_heap) > k:
+            heapq.heappop(max_heap)
+    
+    sol = []
+    while max_heap:
+        val, point = max_heap.pop()
+        sol.append(point)
+
+    return sol
+        
+
+
 
 def main():
     pass
