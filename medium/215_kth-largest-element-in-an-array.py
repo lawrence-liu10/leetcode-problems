bc118d8504f6e0c2abb42307b7b1dfa642ceb249
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-04 15:51:37 -0400

    add heap solution to kth largest element in arr

--- medium/215_kth-largest-element-in-an-array.py
+++ medium/215_kth-largest-element-in-an-array.py
@@ -3,8 +3,21 @@
 
 # Link: https://leetcode.com/problems/kth-largest-element-in-an-array/description/
 
-def solution():
-    pass
+import heapq
+
+# we can just negate each number, heapify, and then return the negated kth popped element
+# time complexity: O(n) negation, O(n) heapify, O(klogn) heappop, so O(n + klogn)
+# O(n) space for the negated list
+def solution(self, nums: list[int], k: int) -> int:
+    
+    nums = [-x for x in nums]
+    heapq.heapify(nums)
+    sol = 0
+    for i in range(k):
+        sol = heapq.heappop(nums)
+    
+    return sol
+
 
 def main():
     pass
