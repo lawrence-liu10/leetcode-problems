179dcaf481e5d0dde65839a6e6a405a76a286fe5
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-05 16:14:57 -0400

    start task scheduler

---
+++ medium/621_task-scheduler.py
@@ -0,0 +1,13 @@
+# 621. Task Scheduler
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/task-scheduler/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
