11b9fcda16992bc24759b1079f864fac4a51ba38
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-06 18:43:44 -0400

    start number of islands

---
+++ medium/200_number-of-islands.py
@@ -0,0 +1,13 @@
+# 200. Number Of Islands
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/number-of-islands/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
