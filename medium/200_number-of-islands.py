4dcd76eb8481ba7228cc6697a98c0722822a03c3
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-06 19:00:30 -0400

    add dfs solution to number of islands

--- medium/200_number-of-islands.py
+++ medium/200_number-of-islands.py
@@ -3,8 +3,37 @@
 
 # Link: https://leetcode.com/problems/number-of-islands/description/
 
-def solution():
-    pass
+# let's go through with a dfs, and at each step, check if a square is a one or zero
+# if it's a one, we'll set it to a zero and keep exploring around it
+# if it's a zero, return
+# after we've gone around the whole area and there's only zeros, we'll increment our island count
+# we have to run dfs starting from every single square though, there might be more efficient approaches
+def solution(self, grid: list[list[str]]) -> int:
+    
+    count = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    def dfs(row, col):
+
+        if (row < 0 or row >= rows or
+            col < 0 or col >= cols or
+            grid[row][col] == "0"):
+            return
+        
+        grid[row][col] == "0"
+        
+        dfs(row - 1, col)
+        dfs(row + 1, col)
+        dfs(row, col - 1)
+        dfs(row, col + 1)
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == "1":
+                dfs(row, col)
+                count += 1
+    
+    return count
 
 def main():
     pass
