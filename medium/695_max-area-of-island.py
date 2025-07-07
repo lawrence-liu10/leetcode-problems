91bdd897831a6322669388790e7ac236b26a1c31
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-07 16:46:38 -0400

    add dfs solution to max area of island

--- medium/695_max-area-of-island.py
+++ medium/695_max-area-of-island.py
@@ -3,8 +3,40 @@
 
 # Link: https://leetcode.com/problems/max-area-of-island/description/
 
-def solution():
-    pass
+# it's similar to number of islands
+# we can run dfs from each square, and when we find an island, we explore each cell and set to zero,
+# incrementing the island size
+def solution(self, grid: list[list[int]]) -> int:
+    
+    rows = len(grid)
+    cols = len(grid[0])
+
+    max_area = 0
+
+    def dfs(row, col) -> int:
+
+        if (row < 0 or col < 0 or
+            row >= rows or col >= cols or
+            grid[row][col] == 0):
+            return 0
+        
+        grid[row][col] = 0
+
+        return (1 + dfs(row - 1, col) +
+                dfs(row + 1, col) +
+                dfs(row, col - 1) + 
+                dfs(row, col + 1))
+    
+    for row in range(rows):
+        for col in range(cols):
+            size = dfs(row, col)
+            max_area = max(max_area, size)
+
+    return max_area
+
+
+    
+
 
 def main():
     pass
