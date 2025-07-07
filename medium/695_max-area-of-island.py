a8de56509949a2b14d8bf4ec8ee926f18bab6b0e
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-07 16:23:11 -0400

    start max area of island

---
+++ medium/695_max-area-of-island.py
@@ -0,0 +1,13 @@
+# 695. Max Area Of Island
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/max-area-of-island/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
