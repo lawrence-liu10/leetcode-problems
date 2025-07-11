95f0bced3acaa6bf0cec357951188e4ccd7a8580
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-11 15:01:27 -0400

    start surrounded regions

---
+++ medium/130_surrounded-regions.py
@@ -0,0 +1,13 @@
+# 130. Surrounded Regions
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/surrounded-regions/description/
+
+def solution(self, board: list[list[str]]) -> None:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
