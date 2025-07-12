5eb17f5a9ae8408c230e1a5722ba0f72a7ff8069
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-12 18:46:38 -0400

    add backtracking sol to course schedule

--- medium/207_course-schedule.py
+++ medium/207_course-schedule.py
@@ -3,8 +3,44 @@
 
 # Link: https://leetcode.com/problems/course-schedule/description/
 
-def solution():
-    pass
+from collections import defaultdict
+
+# it looks like a cycle detection question where we're given node edges
+
+# we can solve it using a defaultdict to define pairs,
+#   and loop through the pairs, keeping track of the processed nodes
+# if we explore from a source node and see a node we've visited on our current backtrack, there's a cycle
+# if we see one we've previously cleared as not having a cycle, continue
+# then check from 0 to numCourses - 1 for cycles
+def solution(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+    
+    graph = defaultdict(list)
+    for dest, src in prerequisites:
+        graph[src].append(dest)
+
+    visited = [0] * numCourses
+    def has_cycle(node):
+        if visited[node] == 1: # current backtrack
+            return True
+        
+        if visited[node] == 2: # previously cleared, no cycle
+            return False
+        
+        visited[node] = 1
+
+        for neighbor in graph[node]:
+            if has_cycle(neighbor):
+                return True
+        
+        visited[node] = 2
+        return False
+    
+    for course in range(numCourses):
+        if has_cycle(course):
+            return False
+        
+    return True
+
 
 def main():
     pass
