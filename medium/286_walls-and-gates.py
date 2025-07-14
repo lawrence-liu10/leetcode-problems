5d7641417e28768dc07826ab3ca03cce313eb1f5
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-14 16:33:53 -0400

    start walls and gates

---
+++ medium/286_walls-and-gates.py
@@ -0,0 +1,13 @@
+# 286. Walls And Gates
+# Difficulty: Medium
+
+# Link: https://neetcode.io/problems/islands-and-treasure
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
