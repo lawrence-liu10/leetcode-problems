9b324b085ee3c1889ba612e81d2868d4288afe57
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-15 16:47:32 -0400

    add dfs solution to connected components

--- medium/323_number-of-connected-components-in-an-undirected-graph.py
+++ medium/323_number-of-connected-components-in-an-undirected-graph.py
@@ -3,8 +3,34 @@
 
 # Link: https://neetcode.io/solutions/number-of-connected-components-in-an-undirected-graph
 
-def solution():
-    pass
+from collections import defaultdict
+
+# we can solve this with a dfs using a visited set
+#   we start from 0 and follow all outbound edges, adding them to our visited arr
+#   each time we start dfs from a non-visited node, we add one to our count
+def solution(self, n: int, edges: list[list[int]]) -> int:
+    
+    graph = defaultdict(list)
+    visited = set()
+    count = 0
+
+    for a, b in edges:
+        graph[a].append(b)
+        graph[b].append(a)
+
+    def dfs(cur_node):
+        if cur_node in visited:
+            return
+        visited.add(cur_node)
+        for neighbor in graph[cur_node]:
+            dfs(neighbor)
+
+    for i in range(n):
+        if i not in visited:
+            dfs(i)
+            count += 1
+    return count
+
 
 def main():
     pass
