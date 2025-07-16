3ee6d6d2d2ce6263841ad147b5de5790df990c0e
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-16 17:51:48 -0400

    start graph valid tree

---
+++ medium/261_graph-valid-tree.py
@@ -0,0 +1,13 @@
+# 261. Graph Valid Tree
+# Difficulty: Medium
+
+# Link: https://neetcode.io/problems/valid-tree
+
+def solution(self, n: int, edges: list[list[int]]) -> bool:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
