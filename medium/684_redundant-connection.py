bd16813557ea54f1f90188bfbaecfc7f1cb134ba
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-17 18:41:59 -0400

    start redundant connection

---
+++ medium/684_redundant-connection.py
@@ -0,0 +1,13 @@
+# 684. Redundant Connection
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/redundant-connection/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
