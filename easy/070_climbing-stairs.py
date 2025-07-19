b1a847bf6c18999f61d4a7fe026d0ce01cc2689d
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-18 23:25:18 -0400

    start climbing stairs

---
+++ easy/070_climbing-stairs.py
@@ -0,0 +1,13 @@
+# 070. Climbing Stairs
+# Difficulty: Easy
+
+# Link: https://leetcode.com/problems/climbing-stairs/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
