75c8e87bca9d4b57e8135eeaa6f99864f6d134a7
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-20 19:49:26 -0400

    start house robber

---
+++ medium/198_house-robber.py
@@ -0,0 +1,13 @@
+# 198. House Robber
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/house-robber/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
