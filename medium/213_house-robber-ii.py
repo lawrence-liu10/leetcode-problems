b636581b529979e111a73c9ac947400370a0e382
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-21 23:43:28 -0400

    add dp solution to house robber ii

--- medium/213_house-robber-ii.py
+++ medium/213_house-robber-ii.py
@@ -3,8 +3,25 @@
 
 # Link: https://leetcode.com/problems/house-robber-ii/description/
 
-def solution():
-    pass
+# extremely similar to house robber
+# we'll just add one more check, where we won't rob the last house if it started on the first house and vice versa
+#   otherwise it's the same. it's not that elegant, but I'll do that for now
+def solution(self, nums: list[int]) -> int:
+    
+    if len(nums) == 1:
+        return nums[0]
+    
+    def rob(sub_arr):
+        prev2, prev1 = 0, 0
+        for num in sub_arr:
+            temp = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = temp
+        return prev1
+    
+    return max(rob(nums[1:]), rob(nums[:-1]))
+
+
 
 def main():
     pass
