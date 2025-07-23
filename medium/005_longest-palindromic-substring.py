443f08509dd12feae1f284e54b369802873cb392
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-22 22:12:39 -0400

    start longest palindromic substring

---
+++ medium/005_longest-palindromic-substring.py
@@ -0,0 +1,13 @@
+# 005. Longest Palindromic Substring
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/longest-palindromic-substring/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
