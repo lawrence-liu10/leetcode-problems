c103a5a2c1a67cb449e70328d0b8043fec90a217
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-25 13:53:39 -0400

    start coin change

---
+++ medium/322_coin-change.py
@@ -0,0 +1,14 @@
+# 322. Coin Change
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/coin-change/
+
+# 
+def solution(self, coins: list[int], amount: int) -> int:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
