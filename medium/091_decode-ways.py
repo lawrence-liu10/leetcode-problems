0d80bc207584a9c7fc383e8234b2c3bf6899b6af
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-24 21:56:41 -0400

    start decode ways

---
+++ medium/091_decode-ways.py
@@ -0,0 +1,13 @@
+# 091. Decode Ways
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/decode-ways/description/
+
+def solution(self, s: str) -> int:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
