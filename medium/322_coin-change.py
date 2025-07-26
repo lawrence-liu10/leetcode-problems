b7e2b62de448a633f82e35b6771ad811da88938c
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-25 21:36:25 -0400

    add dp solution to coin change

--- medium/322_coin-change.py
+++ medium/322_coin-change.py
@@ -3,9 +3,31 @@
 
 # Link: https://leetcode.com/problems/coin-change/
 
-# 
+# to do this question, lets do a dynamic programming approach
+# we'll keep track of the remaining coins required to make the correct change,
+# and go through each coin in coins to recursively calculate down to the target
 def solution(self, coins: list[int], amount: int) -> int:
-    pass
+    memo = {}
+
+    def dp(remaining):
+        if remaining in memo:
+            return memo[remaining]
+        if remaining == 0:
+            return 0
+        if remaining < 0:
+            return -1
+        
+        min_coins = float('inf')
+        for coin in coins:
+            sol = dp(remaining - coin)
+            if sol >= 0:
+                min_coins = min(min_coins, sol + 1)
+        
+        memo[remaining] = min_coins if min_coins != float('inf') else -1
+        return memo[remaining]
+    
+    return dp(amount)
+        
 
 def main():
     pass
