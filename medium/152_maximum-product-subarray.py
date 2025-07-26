2da4a24c41acbf9563dea294456b8d662da2ffbb
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-26 16:23:03 -0400

    start max product subarray

---
+++ medium/152_maximum-product-subarray.py
@@ -0,0 +1,13 @@
+# 152. Maximum Product Subarray
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/maximum-product-subarray/description/
+
+def solution(self, nums: list[int]) -> int:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
