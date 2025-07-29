c072685e5af972290c14b0095cb85d87d0dbdbd9
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-29 01:20:07 -0400

    add dp solutions to longest increasing subsequence

---
+++ medium/300_longest-increasing-subsequence.py
@@ -0,0 +1,43 @@
+# 300. Longest Increasing Subsequence
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/longest-increasing-subsequence/
+
+# we'll solve using dp
+#   we'll memoize our past results
+#   and at each i, we'll choose to either include (add 1 to length) or skip it before continuing
+#   when we reach the end of the list, return
+#   the top down approach tests out of memory
+def solution(self, nums: list[int]) -> int:
+
+    memo = {}
+    def dp(prev, i):
+        if i == len(nums):
+            return 0
+        if (prev, i) in memo:
+            return memo[(prev, i)]
+        taken = 0
+        if prev == -1 or nums[i] > nums[prev]:
+            taken = 1 + dp(i, i + 1)
+        not_taken = dp(prev, i + 1)
+        memo[(prev, i)] = max(taken, not_taken)
+        return memo[(prev, i)]
+    
+    return dp(-1, 0)
+
+# we can convert to a bottom up by using a dp table
+#   we'll explicitly check up to i from the beginning and update the table when necessary
+def bottom_up(self, nums: list[int]) -> int:
+
+    dp = [1] * len(nums)
+    for i in range(1, len(nums)):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
