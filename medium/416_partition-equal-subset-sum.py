7810c3d6c331ae807510794468ff333ea377b5d6
lawrenceliu <lawrence.liu10@gmail.com>
2025-07-29 20:50:03 -0400

    add top down solution to partition equal subset sum

--- medium/416_partition-equal-subset-sum.py
+++ medium/416_partition-equal-subset-sum.py
@@ -1,10 +1,38 @@
 # 416. Partition Equal Subset Sum
 # Difficulty: Medium
 
-# Link: 
+# Link: https://leetcode.com/problems/partition-equal-subset-sum/description/
 
-def solution():
-    pass
+# first, the numbers must be even to be split evenly
+# otherwise, we can loop through the array choosing to either include a number or not
+#   if a sum eventually equals half the total sum, then it can be evenly split
+#   we'll memoize to reduce calculations
+def solution(self, nums: list[int]) -> bool:
+    total = sum(nums)
+    if total % 2 != 0:
+        return False
+    
+    target = total // 2
+    memo = {}
+
+    def dp(i, remaining):
+        if remaining == 0:
+            return True
+        if i >= len(nums) or remaining < 0:
+            return False
+        if (i, remaining) in memo:
+            return memo[(i, remaining)]
+        
+        memo[(i, remaining)] = dp(i + 1, remaining - nums[i]) or dp(i + 1, remaining)
+        return memo[(i, remaining)]
+    
+    return dp(0, target)
+        
+        
+        
+        
+        
+    return dp(0, 0)
 
 def main():
     pass
