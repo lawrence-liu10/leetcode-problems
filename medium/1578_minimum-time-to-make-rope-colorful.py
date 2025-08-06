36535dda4a52b294e12c253a30d708afefeb4629
lawrenceliu <lawrence.liu10@gmail.com>
2025-08-05 22:18:37 -0400

    start min time to make rope colorful

---
+++ medium/1578_minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,13 @@
+# 1578. Minimum Time To Make Rope Colorful
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/minimum-time-to-make-rope-colorful/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
