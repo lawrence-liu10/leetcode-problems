094b7b5883e96f5f5562d5a0ed2f7accc343e68f
lawrenceliu <lawrence.liu10@gmail.com>
2025-08-07 00:16:37 -0400

    add dp solution to max subarray

--- medium/053_maximum-subarray.py
+++ medium/053_maximum-subarray.py
@@ -3,8 +3,18 @@
 
 # Link: https://leetcode.com/problems/maximum-subarray/
 
+# for this question, this is what I'm thinking
+#   we'll keep a running sum, and if it ever goes negative, we'll reset it to the current num, b/c
+#   negative sums only decrease future ones
+# otherwise, update the max sum as needed
 def solution(self, nums: list[int]) -> int:
-    pass
+    cur_sum = max_sum = nums[0]
+
+    for num in nums[1:]:
+        cur_sum = max(num, cur_sum + num)
+        max_sum = max(cur_sum, max_sum)
+    
+    return max_sum
 
 def main():
     pass
