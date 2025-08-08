39f7ef609108dc8b397457ebf547a7e6dad05fbd
lawrenceliu <lawrence.liu10@gmail.com>
2025-08-08 02:45:45 -0400

    add greedy sol to jump game

--- medium/055_jump-game.py
+++ medium/055_jump-game.py
@@ -3,8 +3,17 @@
 
 # Link: https://leetcode.com/problems/jump-game/description/
 
-def solution():
-    pass
+# fun solution
+# we can keep track of the maximum reachable index, and if we ever pass that, we can't reach the end
+#   otherwise, if we reach the last index then we're good
+def solution(self, nums: list[int]) -> bool:
+    max_ind = 0
+    for i in range(len(nums)):
+        if i > max_ind:
+            return False
+        if i == len(nums) - 1:
+            return True
+        max_ind = max(i + nums[i], max_ind)
 
 def main():
     pass
