a58b1a70b5dba9bc66fe9474693d609dd8a6aac1
lawrenceliu <lawrence.liu10@gmail.com>
2025-08-08 02:29:41 -0400

    start jump game

---
+++ medium/055_jump-game.py
@@ -0,0 +1,13 @@
+# 055. Jump Game
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/jump-game/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
