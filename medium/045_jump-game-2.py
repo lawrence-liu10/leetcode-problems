ee2b46b2851bdc10d28bc50b34a727a393abb48d
loogway <lawrence.liu10@gmail.com>
2025-08-09 21:44:30 -0400

    start jump game 2

---
+++ medium/045_jump-game-2.py
@@ -0,0 +1,13 @@
+# 045. Jump Game 2
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/jump-game-ii/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
