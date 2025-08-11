e6642a864ebe45e648a0e7fa3c15293ae363ac49
loogway <lawrence.liu10@gmail.com>
2025-08-10 23:53:17 -0400

    add greedy sol to gas station

--- medium/134_gas-station.py
+++ medium/134_gas-station.py
@@ -1,10 +1,33 @@
 # 134. Gas Station
 # Difficulty: Medium
 
-# Link:
+# Link: https://leetcode.com/problems/gas-station/description/
 
-def solution():
-    pass
+
+# the brute force is simple, just loop through the array from each index
+#   what's a more efficient approach
+
+# first the cost must be less than or equal to the total gas, otherwise it's impossible
+#   similar to the max subarray question, if a portion of the array causes
+#   the tank to become negative, then it can't be the starting value, and the start must come after
+#   keep going until we don't need to reset
+
+def solution(self, gas: list[int], cost: list[int]) -> int:
+    if sum(gas) < sum(cost):
+        return -1
+
+    cur = 0
+    start_ind = 0
+    for i in range(len(gas)):
+        cur += gas[i] - cost[i]
+
+        if cur < 0:
+            cur = 0
+            start_ind = i + 1
+
+    return start_ind
+
+
 
 def main():
     pass
