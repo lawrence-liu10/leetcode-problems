4d9290621f40cdd6aebd12eda5244311e950a888
loogway <lawrence.liu10@gmail.com>
2025-08-11 23:59:50 -0400

    start hand of straights

---
+++ medium/846_hand-of-straights.py
@@ -0,0 +1,14 @@
+# 846. Hand Of Straights
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/hand-of-straights/description/
+
+def solution(self, hand: list[int], groupSize: int) -> bool:
+
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
