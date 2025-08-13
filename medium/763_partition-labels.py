b4344a477d1a4816ed2333c7e45c63b48de473e8
loogway <lawrence.liu10@gmail.com>
2025-08-13 18:18:43 -0400

    Add greedy sol to partition labels

--- medium/763_partition-labels.py
+++ medium/763_partition-labels.py
@@ -3,8 +3,30 @@
 
 # Link: https://leetcode.com/problems/partition-labels/description/
 
-def solution():
-    pass
+# we need the first letter to only appear in its own section,
+#   we'll take a right pointer and set it to the last appearance of the left letter
+#   as well, we'll find which char from left to right pointer appears the latest in the string,
+#       using that as the point to partition from
+# we'll continue with that process until the end
+def solution(self, s: str) -> list[int]:
+    sol = []
+    left = 0
+    while left < len(s):
+        right = s.rindex(s[left])
+
+        cur = left + 1
+        while cur < right:
+            rightC = s.rindex(s[cur])
+            if rightC > right:
+                right = rightC
+            cur += 1
+
+        sol.append(right - left + 1)
+        left = right + 1
+    return sol
+
+
+
 
 def main():
     pass
