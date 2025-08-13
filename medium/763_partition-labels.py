8e069193b1d6eab9caa7403f2d324151a40d0ce2
loogway <lawrence.liu10@gmail.com>
2025-08-13 18:00:12 -0400

    start partition labels

---
+++ medium/763_partition-labels.py
@@ -0,0 +1,13 @@
+# 763. Partition Labels
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/partition-labels/description/
+
+def solution():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
