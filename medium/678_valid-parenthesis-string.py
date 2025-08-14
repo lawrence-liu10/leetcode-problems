a106b793f368042b4bad9937914beee8fcecf828
loogway <lawrence.liu10@gmail.com>
2025-08-14 17:53:57 -0400

    start valid parenthesis string

---
+++ medium/678_valid-parenthesis-string.py
@@ -0,0 +1,13 @@
+# 678. Valid Parenthesis String
+# Difficulty: Medium
+
+# Link: https://leetcode.com/problems/valid-parenthesis-string/description/
+
+def solution(self, s: str) -> bool:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
